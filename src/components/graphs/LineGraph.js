563be294ccd6c1b479eee3f4d26ffb63939d4d42
dumidu1998 <dumiduraj@gmail.com>
2021-10-25 22:12:37 +0530

    graph container added

--- src/components/graphs/LineGraph.js
+++ src/components/graphs/LineGraph.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function LineGraph() {
     return (
-        <div className="flex flex-col col-span-1 md:col-span-2 sm:col-span-4 xl:col-span-1 bg-white shadow-lg rounded-sm border border-gray-200">
-            <h1 className="text-2xl font-semibold">Overview Expenditure</h1>
+        <div className="flex flex-col col-span-full md:col-span-2 sm:col-span-4 xl:col-span-1 bg-white shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-100 flex justify-start">
+                <h1 className="text-2xl font-semibold">Expenditure Overview</h1>
+            </header>
         </div>
     )
 }
-
 export default LineGraph
--- src/components/graphs/PieChart.js
+++ src/components/graphs/PieChart.js
@@ -3,7 +3,9 @@ import React from 'react'
 function PieChart() {
     return (
         <div className="flex flex-col col-span-full md:col-span-2 sm:col-span-4 xl:col-span-1 bg-white shadow-lg rounded-sm border border-gray-200">
-            <h1 className="text-2xl font-semibold">Expenditures by Category</h1>
+            <header className="px-5 py-4 border-b border-gray-100 flex justify-start">
+                <h1 className="text-2xl font-semibold">Expenditures by Category</h1>
+            </header>
 
         </div>
     )
