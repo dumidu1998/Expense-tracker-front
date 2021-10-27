3d7caf0c98b0e9d0ab8e7fa1493b69196717c3ee
dumidu1998 <dumiduraj@gmail.com>
2021-10-27 13:56:02 +0530

    icon changed

--- src/components/cards/Card4.js
+++ src/components/cards/Card4.js
@@ -38,7 +38,7 @@ function Card4() {
                     <div className="flex flex-row items-center">
                         <h2 className="text-3xl flex font-semibold text-gray-800 mb-2 bg-blue-200 p-3">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </h2>
                         <div className="flex flex-col ml-4">
